2a1bbf78310d060516899cd1ef89862c422f7e80
zhuzedan <1031155817@qq.com>
2023-03-20 21:55:43 +0800

    feat: 菜单栏通过数组循环出来

---
+++ src/components/common/BasicLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <el-container>
+      <!--侧栏-->
+      <el-aside width="auto">
+        <aside-layout></aside-layout>
+      </el-aside>
+      <el-container>
+        <!--头部-->
+        <el-header>Header</el-header>
+        <!--内容-->
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import AsideLayout from "../common/AsideLayout";
+export default {
+  name: "BasicLayout",
+  components: {
+    AsideLayout
+  },
+
+  data() {
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/common/HeaderLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "HeaderLayout"
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
